248573588f6b8013f978460351fdda74f7817aa5
Shaade <s.shaade77@gmail.com>
2022-04-10 12:13:59 +0200

    ensure redis connects, quits otherwise

--- app/cache.py
+++ app/cache.py
@@ -1,6 +1,16 @@
 import redis as cache_server
+
 from .config import config
+from app.utilities import logger
 
 # no clue how to call it for now,
 
 redis = cache_server.Redis(host=config.REDIS_HOST, port=config.REDIS_PORT, db=config.REDIS_DB, decode_responses=True)
+
+
+try:
+    redis.ping()
+except Exception as e:
+    logger.critical(e)
+    logger.critical("Redis is not running. Please start it.")
+    exit(1)
