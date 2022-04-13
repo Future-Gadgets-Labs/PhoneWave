1dd425a89741ab223d28a66c7c4c3b66e73b0918
Shaade <s.shaade77@gmail.com>
2022-04-13 08:35:26 +0200

    #2

--- app/extentions/admin/admin.py
+++ app/extentions/admin/admin.py
@@ -5,9 +5,6 @@ from app.utilities import logger
 
 
 class AdminExtention(BaseCog):
-    def __init__(self, bot):
-        super().__init__(bot)
-
     @BaseCog.listener()
     async def on_message(self, member):
         print("new message from " + member.name)
