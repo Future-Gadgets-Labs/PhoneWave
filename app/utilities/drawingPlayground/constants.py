1c016b2206682eaf7a0f389c4cd849e4c32e9ef9
Dolidodzik <teethtard321@gmail.com>
2022-04-25 16:05:42 +0200

    created basic setup for new profile card

---
+++ app/utilities/drawingPlayground/constants.py
@@ -0,0 +1,20 @@
+# CONSTANTS THAT ARE USED FOR drawing/
+# THEY DEFINE PLACEMENTS/COLORS AND DEFAULT FONTS/IMAGES PATHS
+
+
+# SIZES
+background_dimensions = (419, 404) # desired dimensions of card you want to crop out from 513x513 square
+
+
+# OFFSETS
+
+
+
+# COLORS
+
+
+# IMAGE PATHS
+server_icon_path = 'server_icon.png'
+
+# FONTS
+default_font = "fonts/Nunito-VariableFont_wght.ttf"
---
+++ app/utilities/drawingPlayground/master.py
@@ -0,0 +1,42 @@
+import sys
+import os
+import PIL
+from PIL import Image, ImageDraw, ImageOps, ImageFont, ImageEnhance, ImageFilter
+from pathlib import Path
+import requests
+from io import BytesIO
+
+from utils import *
+import constants
+
+# changing import path
+abspath = os.path.abspath(__file__)
+ipath = os.path.dirname(abspath)
+os.chdir(ipath)
+
+
+def drawProfileCard(avatar_url, nickname, discriminator, labmem_number, level, rank, messages_sent, badges_list):
+    # getting images that can be ever needed
+    pfp_original = Image.open( requests.get(avatar_url, stream=True).raw )
+    background = PIL.Image.new(
+        mode="RGBA",
+        size=constants.background_dimensions,
+        color=(0, 0, 0, 255)
+    )
+
+
+    # merging full_background (the one with alpha around it) with background (the one with all card contents in it)
+    profile_card = background
+    profile_card.save("final_output.png")
+
+
+drawProfileCard(
+    "https://cdn.discordapp.com/avatars/487896060316876800/b603f6cce63f7c6430559ae5c3a00f4b.png?size=512",
+    "FreshTeaBagsByLipton",
+    2036,
+    222,
+    21,
+    1,
+    235000,
+    ['operation_elysian_veteran', 'daru69']
+)
