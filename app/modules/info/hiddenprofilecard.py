622f254d254cd773bced06c24a689bf2cf9dbe9b
Dolidodzik <teethtard321@gmail.com>
2022-04-28 17:51:16 +0200

    removed unused server path icon

--- app/modules/info/hiddenprofilecard.py
+++ app/modules/info/hiddenprofilecard.py
@@ -16,42 +16,45 @@ def profileCardFromMember(member):
     if not nickname:
         nickname = member.name
 
-
-
     #######################################################
     #  TODO!!! REPLACE PLACEHOLDER VALUES WITH REAL ONES  #
     #######################################################
 
     return discord.File(
         drawing.drawProfileCard(
-            member.avatar.url, # avatar
-            nickname, # nickname
-            member.discriminator, # discriminator
-            222, # lab mem
-            21, # level
-            1, # rank
-            235621, # messages sent
-            54200, # xp current
-            60000, # next level xp
-            ['operation_elysian_veteran', 'daru69'] # Acquired badges names, they represent file as shown in constants.badges_map
-        ), filename="profile_card.png"
+            member.avatar.url,  # avatar
+            nickname,  # nickname
+            member.discriminator,  # discriminator
+            222,  # lab mem
+            21,  # level
+            1,  # rank
+            235621,  # messages sent
+            54200,  # xp current
+            60000,  # next level xp
+            [
+                "operation_elysian_veteran",
+                "daru69",
+            ],  # Acquired badges names, they represent file as shown in constants.badges_map
+        ),
+        filename="profile_card.png",
     )
+
 
 class Hiddenprofilecard(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    #async def hiddenprofilecard(self, ctx: Context):
+    # async def hiddenprofilecard(self, ctx: Context):
     async def hpc(self, ctx: Context, member: discord.Member = None):
         logger.info("Received 'Hiddenprofilecard' command...")
 
         # checking if any mentions were sent
         if ctx.message.mentions:
             for mention in ctx.message.mentions:
-                await ctx.send(file = profileCardFromMember(mention))
+                await ctx.send(file=profileCardFromMember(mention))
         else:
-            await ctx.send(file = profileCardFromMember(ctx.message.author))
+            await ctx.send(file=profileCardFromMember(ctx.message.author))
         return True
 
 
